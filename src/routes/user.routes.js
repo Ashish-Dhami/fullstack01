28bb166f18101bf5e10c37ed23708a4010e43c32
Ashish Dhami <ashish.dhami.career@gmail.com>
2024-11-20 14:38:15 +0530

    prettier

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,25 +9,25 @@ import {
   updateAccountDetails,
   updateUserAvatar,
   getUserChannelProfile,
-  getWatchHistory
-} from "../controllers/user.controller.js"
+  getWatchHistory,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { isLoggedIn } from "../middlewares/auth.middleware.js";
 
-const router = Router()
-router.route("/register").post(upload.fields(
-  [
-    { name: "avatar" }
-  ]
-),registerUser)
-router.route("/login").post(loginUser)
-router.route("/logout").post(isLoggedIn,logoutUser)
-router.route("/refreshAccessToken").post(refreshAccessToken)
-router.route("/changePassword").post(isLoggedIn,changePassword)
-router.route("/currentUser").get(isLoggedIn,getCurrentUser)
-router.route("/updateDetails").patch(isLoggedIn,updateAccountDetails)
-router.route("/updateAvatar").patch(isLoggedIn,upload.single("avatar"),updateUserAvatar)
-router.route("/userProfile/:username").get(isLoggedIn,getUserChannelProfile)
-router.route("/watchHistory").get(isLoggedIn,getWatchHistory)
+const router = Router();
+router
+  .route("/register")
+  .post(upload.fields([{ name: "avatar" }]), registerUser);
+router.route("/login").post(loginUser);
+router.route("/logout").post(isLoggedIn, logoutUser);
+router.route("/refreshAccessToken").post(refreshAccessToken);
+router.route("/changePassword").post(isLoggedIn, changePassword);
+router.route("/currentUser").get(isLoggedIn, getCurrentUser);
+router.route("/updateDetails").patch(isLoggedIn, updateAccountDetails);
+router
+  .route("/updateAvatar")
+  .patch(isLoggedIn, upload.single("avatar"), updateUserAvatar);
+router.route("/userProfile/:username").get(isLoggedIn, getUserChannelProfile);
+router.route("/watchHistory").get(isLoggedIn, getWatchHistory);
 
-export default router
+export default router;
